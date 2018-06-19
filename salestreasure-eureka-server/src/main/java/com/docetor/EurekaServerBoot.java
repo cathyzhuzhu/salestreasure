185cf3f66c4115138ce1346d54e08c05bcc40c89
HuangJuan <huangj@lensyn.com>
2018-06-19 20:39:15 +0800

    初始化注册中心

---
+++ salestreasure-eureka-server/src/main/java/com/docetor/EurekaServerBoot.java
@@ -0,0 +1,15 @@
+package com.docetor;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerBoot {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(EurekaServerBoot.class).web(true).run(args);
+	}
+
+}
